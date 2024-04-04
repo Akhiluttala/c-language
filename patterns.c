03f223b94293c73970c6de2ad96007f9db882a81
akhil <akhil20062000>
2024-04-04 10:20:02 +0530

    added patterns

--- patterns.c
+++ patterns.c
@@ -3,9 +3,10 @@ int main()
 {
     int i,n;
     printf("enter the value");
-    scanf("%d,&n");
-    for(i=2;i<=n;i+2);
+    scanf("%d",&n);
+    for(i=2;i<=n;i++)
     {
-        printf("%d",i);
+        printf("%d\n",i);
     }
+    return 0;
 }
